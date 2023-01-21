55f1df4d7878e08fcaa87e8173646f0161f70460
Serhii <example@gmail.com>
2023-01-21 17:16:34 +0200

    New function

--- js/script.js
+++ js/script.js
@@ -1,95 +1,111 @@
-'use script';
+(function() {
 
-const container = document.querySelector('.slider__container');
-const prev = document.querySelector('.slider__arr-prev');
-const next = document.querySelector('.slider__arr-next');
-const slider = document.querySelector('.slider');
-const wrapper = document.querySelector('.slider__wrapper');
-const width = window.getComputedStyle(wrapper).width;
-const slidesInner = document.querySelector('.slider__inner');
-const slides = document.querySelectorAll('.slide');
+    function initSlider() {
+        createCounterBlock();
+        arrowNextSlide();
+        arrowPrevSlide();
+        activeCounterBlock();
+    }
+ 
+    const wrapper = document.querySelector('.slider__wrapper');
+    const width = window.getComputedStyle(wrapper).width;
+    const slidesInner = document.querySelector('.slider__inner');
+    const slides = document.querySelectorAll('.slide');
 
-let slideId = 1;
-let slidePx = 0;
+    let activeSlide = 1;
+    let slideOffsetX = 0;
 
-function removeLetters(num) {
-    return +num.replace(/\D/g, '');
-}
+    slidesInner.style.width = 100 * slides.length + '%';
 
-slidesInner.style.width = 100 * slides.length + '%';
+    slides.forEach(item => item.style.width = width);
 
-slides.forEach(item => item.style.width = width);
+    const slidesCounter = [];
 
-const blockCounter = document.createElement('div');
-const slidesCounter = [];
+    function createCounterBlock() {
+        const container = document.querySelector('.slider__container');
+        const blockCounter = document.createElement('div');
+        
+        blockCounter.classList.add('counter__block');
+        container.append(blockCounter);
 
-blockCounter.classList.add('counter__block');
-container.append(blockCounter);
-
-for (let i = 0; i < slides.length; i++) {
-    const counter = document.createElement('span');
-
-    counter.classList.add('counter');
-    counter.setAttribute('data-slide-i', i + 1);
-    counter.textContent = i + 1;
-
-    if (i == 0) {
-        counter.classList.add('active');
+        for (let i = 0; i < slides.length; i++) {
+            const counter = document.createElement('span');
+        
+            counter.classList.add('counter');
+            counter.textContent = i + 1;
+        
+            if (i == 0) {
+                counter.classList.add('active');
+            }
+        
+            blockCounter.append(counter);
+            
+            slidesCounter.push(counter);
+        }
     }
 
-    blockCounter.append(counter);
+    function arrowNextSlide() {
+        const next = document.querySelector('.slider__arr-next');
+
+        next.addEventListener('click', () => {
+            if (slideOffsetX == parseInt(width) * (slides.length - 1)) {
+                slideOffsetX = 0;
+            } else {
+                slideOffsetX += parseInt(width);
+            }
+        
+            slidesInner.style.transform = `translateX(-${slideOffsetX}px)`;
+        
+            if (activeSlide == slides.length) {
+                activeSlide = 1;
+            } else {
+                activeSlide++;
+            }
+        
+            slidesCounter.forEach(item => item.classList.remove('active'));
+            slidesCounter[activeSlide - 1].classList.add('active');
+        });
+    }
+
+    function arrowPrevSlide() {
+        const prev = document.querySelector('.slider__arr-prev');
+
+        prev.addEventListener('click', () => {
+            if (slideOffsetX == 0) {
+                slideOffsetX = parseInt(width) * (slides.length - 1);
+            } else {
+                slideOffsetX -= parseInt(width);
+            }
+        
+            slidesInner.style.transform = `translateX(-${slideOffsetX}px)`;
+        
+            if (activeSlide == 1) {
+                activeSlide = slides.length;
+            } else {
+                activeSlide--;
+            }
+        
+            slidesCounter.forEach(item => item.classList.remove('active'));
+            slidesCounter[activeSlide - 1].classList.add('active');
+        });
+    }
+
+    function activeCounterBlock() {
+        slidesCounter.forEach(item => {
+            item.addEventListener('click', (e) => {
+                const slideIndex = slidesCounter.indexOf(e.target);
+                
+                activeSlide = slideIndex + 1;
+                slideOffsetX = parseInt(width) * slideIndex;
+
+                slidesInner.style.transform = `translateX(-${slideOffsetX}px)`;
+        
+                slidesCounter.forEach(item => item.classList.remove('active'));
+                slidesCounter[activeSlide - 1].classList.add('active');
+            });
+        });
+    }
     
-    slidesCounter.push(counter);
-}
+    initSlider();
 
-next.addEventListener('click', () => {
-    if (slidePx == removeLetters(width) * (slides.length - 1)) {
-        slidePx = 0;
-    } else {
-        slidePx += removeLetters(width);
-    }
-
-    slidesInner.style.transform = `translateX(-${slidePx}px)`;
-
-    if (slideId == slides.length) {
-        slideId = 1;
-    } else {
-        slideId++;
-    }
-
-    slidesCounter.forEach(item => item.classList.remove('active'));
-    slidesCounter[slideId - 1].classList.add('active');
-});
-
-prev.addEventListener('click', () => {
-    if (slidePx == 0) {
-        slidePx = removeLetters(width) * (slides.length - 1);
-    } else {
-        slidePx -= removeLetters(width);
-    }
-
-    slidesInner.style.transform = `translateX(-${slidePx}px)`;
-
-    if (slideId == 1) {
-        slideId = slides.length;
-    } else {
-        slideId--;
-    }
-
-    slidesCounter.forEach(item => item.classList.remove('active'));
-    slidesCounter[slideId - 1].classList.add('active');
-});
-
-slidesCounter.forEach(item => {
-    item.addEventListener('click', (e) => {
-        const slidesAttribute = e.target.getAttribute('data-slide-i');
-
-        slideId = slidesAttribute;
-        slidePx = removeLetters(width) * (slidesAttribute - 1);
-
-        slidesInner.style.transform = `translateX(-${slidePx}px)`;
-
-        slidesCounter.forEach(item => item.classList.remove('active'));
-        slidesCounter[slideId - 1].classList.add('active');
-    });
-});
+}());
